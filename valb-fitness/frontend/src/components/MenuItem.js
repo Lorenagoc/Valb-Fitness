579d8ee92d85abe64254ffa592df0b6c87310e49
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2022-07-13 23:33:55 -0300

    useFetch to GET from db

--- valb-fitness/frontend/src/components/MenuItem.js
+++ valb-fitness/frontend/src/components/MenuItem.js
@@ -1,8 +1,15 @@
 import React from "react";
+import useFetch from "../hooks/useFetch";
 
 function MenuItem({ image, name, price }) {
+  const {data: planos, isPending, error } = useFetch('http://localhost:3001/Planos');
+  const handleHover = (e) => {
+    e.preventDefault();
+    console.log(planos)
+    
+  }
   return (
-    <div className="menuItem">
+    <div className="menuItem" onMouseEnter={handleHover}>
       <div style={{ backgroundImage: `url(${image})` }}> </div>
       <h1> {name} </h1>
       <p> ${price} </p>
