913099710a545759190d808f04e51e554d5d710e
Lorenagoc <lorenagoc99@gmail.com>
2022-07-15 23:52:15 -0300

    Add helpers

---
+++ valb-fitness/frontend/src/helpers/GenericList.js
@@ -0,0 +1,19 @@
+export const GenericList = [
+    {
+        mensal: [{
+            duas: 0,
+            cinco: 155,
+            sete: 0
+        }],
+        semestral: [{
+            duas: 0,
+            cinco: 139,
+            sete: 0
+        }],
+        anual: [{
+            duas: 0,
+            cinco: 130,
+            sete: 0
+        }]
+    } 
+];
